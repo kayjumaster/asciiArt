54bb6c42bbad7ea41f2843912962d8ee95600840
Benoit <benoit.ferrer@koben.fr>
2016-05-12 12:12:33 +0200

    Lecture des lignes

---
+++ asciiArt.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int L = in.nextInt();
+        int H = in.nextInt();
+        in.nextLine();
+        String T = in.nextLine();
+        
+        System.out.println(L);
+        System.out.println(H);
+        System.out.println(T);
+        
+        for (int i = 0; i < H; i++) {
+            String ROW = in.nextLine();
+        }
+
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("answer");
+    }
+}
