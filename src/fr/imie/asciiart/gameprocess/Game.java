777867d9e57219bd6aa4696692cc08b86107a530
Arthur Alliot <pro.alliot@gmail.com>
2016-05-12 16:45:28 +0200

    commit 1.1.2

--- src/fr/imie/asciiart/gameprocess/Game.java
+++ src/fr/imie/asciiart/gameprocess/Game.java
@@ -6,9 +6,9 @@ public class Game extends User {
 
 	static String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-	public Game(int height, int width, int numbChar) {
+	public Game(int height, int width) {
 
-		// find the necessary indexes of letters
+// find the necessary indexes of letters
 		ArrayList<Integer> indexes = new ArrayList<Integer>();
 		for (char c : text.toCharArray()) {
 			if (c < 'A' || c > 'Z')
@@ -22,7 +22,7 @@ public class Game extends User {
 			}
 		}
 
-		// Affichage du résultat
+// Result display
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < indexes.size(); j++) {
 				System.out.print(asciiTab.get(i).substring(indexes.get(j) * width, indexes.get(j) * width + width));
@@ -33,6 +33,9 @@ public class Game extends User {
 	}
 
 	public static void main(String[] args) {
+		
+		Game game = new Game(8, 8);
+//		System.out.println(game);
 
 	}
 }
